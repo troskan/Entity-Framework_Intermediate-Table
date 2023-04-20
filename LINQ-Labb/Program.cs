3973832e91ee80281f6a8ca39dde85a7a29500f9
Alvin strandberg <Str4ndb3rg@live.se>
2023-04-21 00:32:15 +0200

    added get teacher with student method.

--- LINQ-Labb/Program.cs
+++ LINQ-Labb/Program.cs
@@ -3,235 +3,184 @@ using LINQ_Labb.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.Metrics;
 using System.Security.Cryptography.X509Certificates;
+using LINQ_Labb.Utillity;
 
 namespace LINQ_Labb
 {
     internal class Program
     {
-        public static int CourseCounter = 3;
         static void Main(string[] args)
         {
-            DisplayAllSubjects();
+            //Get all teachers that do math.
+            //// GetAllCourseTeachers();
+            GetAllTeachersWithStudent();
+            //Get all teachers with their connected teacher
 
 
-            DisplayCoursesWithStudents();
-            DisplayAllCourses();
+            //DBMethods.DisplayAll();
+            // DisplayTeacherConnection("Mathematics");
+            //AddTeacherToCourse(4, 1);
+            //AddTeacherToCourse(5, 2);
+            //AddTeacherToCourse(7, 3);
 
+            // DisplayTeacherConnection();
 
 
         }
-        private static void DisplayCoursesWithStudents()
+
+        public static void GetAllTeachersWithStudent()
         {
             using (var db = new Context())
             {
-                var jTable = db.Course.Include(s => s.Student).ToList();
+                //      var query = db.Course
+                //.SelectMany(c => c.Student, (course, student) => new { Course = course, Student = student })
+                //.Join(db.Teacher,
+                //    x => x.Course.Teacher,
+                //    tc => tc.Course,
+                //    (x, tc) => new { Course = x.Course, Student = x.Student, TeacherCourse = tc })
+                //.Join(db.Teacher,
+                //    x => x.TeacherCourse.TeacherId,
+                //    t => t.Id,
+                //    (x, t) => new { Course = x.Course, Student = x.Student, Teacher = t });
 
-                foreach (var c in jTable)
+                //      foreach (var row in query)
+                //      {
+                //          Console.WriteLine($"Course: {row.Course.Name}, Student: {row.Student.FirstName} {row.Student.LastName}, Teacher: {row.Teacher.FirstName} {row.Teacher.LastName}");
+                //      }
+
+
+                //  };
+                var course = db.Course.Include(x => x.Student).Include(t => t.Teacher).ToList();
+
+                foreach (var i in course)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"\t##\t\t\t##");
+                    Console.WriteLine($"\t   Course: {i.Name}");
+                    Console.WriteLine($"\t##\t\t\t##");
+                    Console.WriteLine();
+
+                    foreach (var s in i.Student)
+                    {
+                        if(s.FirstName.Length != 10)
+                        {
+                            for (int c = s.FirstName.Length; c != 10; c++)
+                            {
+                                s.FirstName += " ";
+                            }
+                        }
+                        Console.Write($"Student: {s.FirstName}  Teachers: ");
+
+                        foreach (var c in i.Teacher)
+                        {
+                            Console.Write($"{c.FirstName} ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+
+
+            }
+
+
+            //using (var db = new Context())
+            //{
+            //    var course = db.Course.Include(c => c.Teacher).Include(s => s.Teacher).ToList();
+
+            //    foreach (var co in course)
+            //    {
+            //        Console.WriteLine($"{co.Name}");
+
+            //        foreach (var st in co.Student)
+            //        {
+            //            Console.WriteLine($"Student: {st.FirstName}");
+            //        }
+            //        foreach (var te in co.Teacher)
+            //        {
+            //            Console.WriteLine($"Teacher:{te.FirstName} {te.LastName}");
+            //        }
+            //    }
+            //}
+        }
+            public static void AddTeacherToCourse(int courseID, int teacherID)
+        {
+            using(var db = new Context())
+            {
+                var selectCourse = db.Course.FirstOrDefault(c => c.CourseID == courseID);
+
+                var selectTeacher = db.Teacher.FirstOrDefault(t=>t.TeacherID == teacherID);
+
+                selectCourse.Teacher = new List<Teacher>()
+                {
+                    selectTeacher
+                };
+
+
+                db.SaveChanges();
+
+            }
+        } 
+        public static void RemoveTeacherInCourse(int teacherID)
+        {
+            //using(var db = new Context())
+            //{
+            //    var select = db.Course.Include(c => c.Teacher).ToList();
+
+            //    select.RemoveAll(x => x.CourseID != 0);
+            //    select.
+
+
+            //    db.SaveChanges();
+
+            //}
+        }
+        public static void GetAllCourseTeachers() 
+        {
+            using(var db = new Context())
+            {
+                var course = db.Course.Include(t => t.Teacher).ToList();
+
+                foreach (var c in course)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("****************************************");
                     Console.ForegroundColor = ConsoleColor.Red;
+
+
                     Console.WriteLine($"ID:{c.CourseID} Course: {c.Name}");
+
+
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("****************************************");
                     Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
 
-                    foreach (var s in c.Student)
+                    foreach (var t in c.Teacher)
                     {
-                        Console.WriteLine($"ID: {s.StudentID} Name: {s.FirstName} {s.LastName}");
+                        Console.WriteLine($"ID:{t.TeacherID} Name: {t.FirstName} {t.LastName}");
+                        Console.WriteLine();
                     }
                 }
-                
-
-               
-            }
-        }
-        private static void AddManyStudentsToCourse(int startIndex, int stopIndex, int courseID)
-        {
-            Context db = new Context();
-            var students = db.Student;
-            int counter = 0;
-
-            for (int i = startIndex; i <= stopIndex; i++)
-            {
-                Console.WriteLine($"Added StudentID #{i}");
-                AddStudentToCourse(courseID, i);
-                counter++;
-            }
-            Console.WriteLine($"Attempt to add {counter} students has been made and is finished.");
-        }
-
-        public static void DisplayAllCourses()
-        {
-            Context db = new Context();
-            var courses = db.Course.ToList();
-            Console.WriteLine("************************");
-            Console.WriteLine("Displaying all courses registered in school.");
-            foreach (var c in courses)
-            {
-                Console.WriteLine($"#{c.CourseID} Course: {c.Name}");
-            }
-        }
-        public static void DisplayAllSubjects()
-        {
-            Context db = new Context();
-            var subjects = db.Subject.Include(c=>c.Course).ToList();
-            Console.WriteLine("************************");
-            Console.WriteLine("Displaying all subjects registered in school.");
-            var courseName = "";
-            
-
-            foreach (var c in subjects)
-            {
-                Console.WriteLine($"#{c.SubjectID} Subject: {c.Name} - CourseID: {c.CourseID} Course: {c.Name}");
-            }
-        }
-        public static void DisplayAll()
-        {
-            Context db = new Context();
-            var students = db.Student.ToList();
-            var teachers = db.Teacher.ToList();
-
-            Console.WriteLine("************************");
-            Console.WriteLine("Displaing all persons registered in school.");
-            foreach (var s in students)
-            {
-                Console.WriteLine($"#{s.StudentID} Student: {s.FirstName} {s.LastName}");
-            }
-            Console.WriteLine("-----");
-            foreach (var t in teachers)
-            {
-                Console.WriteLine($"#{t.TeacherID} Teachers: {t.FirstName} {t.LastName}");
-            }
-        }
-
-        public static void AddStudent(string fname,string lname)
-        {
-            Context db = new Context();
-            Student student = new Student();
-
-            student.FirstName = fname;
-            student.LastName = lname;
-           
-            db.Student.Add(student);
-
-            db.SaveChanges();
-            Console.WriteLine($"#{student.StudentID} {student.FirstName} {student.LastName} was added successfully!");
-        } 
-        public static void AddTeacher(string fname,string lname)
-        {
-            Context db = new Context();
-            Teacher teacher = new Teacher();
-
-            teacher.FirstName = fname;
-            teacher.LastName = lname;
-
-            db.Teacher.Add(teacher);
-            db.SaveChanges();
-            Console.WriteLine($"#{teacher.TeacherID} {teacher.FirstName} {teacher.LastName} was added successfully!");
-        }
-        public static void AddCourse(string courseName)
-        {
-            Context db = new Context();
-            Course course = new Course();
-
-            course.Name = courseName;
-
-            db.Course.Add(course);
-            db.SaveChanges();
-
-            Console.WriteLine($"#{course.CourseID} {courseName} was added successfully!");
-        }
-        public static void AddSubject(string subjectName, int courseID)
-        {
-            Context db = new Context();
-            Subject subject = new Subject();
-
-            var courseObj = db.Course.FirstOrDefault(x => x.CourseID == courseID);
-            
-            subject.Name = subjectName;
-            subject.CourseID = courseID;
-            subject.Course = courseObj;
-            
-            db.Subject.Add(subject);
-            db.SaveChanges();
-
-        }
-
-        public static string AddStudentToCourse(int courseID, int studentID)
-        {
-            using (var db = new Context())
-            {
-                var selectCourse = db.Course.FirstOrDefault(x => x.CourseID == courseID);
-
-                var student = db.Student.FirstOrDefault(s => s.StudentID == studentID);
-
-
-                if (db.Student.Contains(db.Student.FirstOrDefault(s => s.StudentID == studentID)))
-                {
-                    selectCourse.Student = new List<Student> { student };
-
-
-                    db.SaveChanges();
-                    Console.WriteLine($"Sucessfully added Student: ID# {student.StudentID}, Name: {student.FirstName}");
-                    return "Successfully added.";
-                }
-
-                Console.WriteLine($"Could not add Student: ID# {student.StudentID}, Name: {student.FirstName}");
-
-                return "Error, could not add.";
 
             }
-
-
-
         }
-        public static void DeleteSubject(int subjectID)
+        public static void DisplayTeacherConnection(string courseName)
         {
-            using (Context db = new Context())
-            {
-                // Retrieve the student with the specified ID
-                Subject subject = db.Subject.FirstOrDefault(s => s.SubjectID == subjectID);
+            //using (var db = new Context())
+            //{
+            //    var course = db.Course.FirstOrDefault(c=>c.Name ==  courseName);
 
-                if (subject != null)
-                {
-                    // Remove the student from the DbSet
-                    db.Subject.Remove(subject);
+            //    foreach (var i in course)
+            //    {
+            //        Console.WriteLine($"Course:{i}, Teachers:");
 
-                    // Save changes to the database
-                    db.SaveChanges();
-                    
-
-                    Console.WriteLine($"#{subject.SubjectID} {subject.Name} was deleted successfully.");
-                }
-                else
-                {
-                    Console.WriteLine($"Subject with ID {subjectID} not found.");
-                }
-            }
-        }
-        public static void DeleteCourse(int courseID)
-        {
-            using (Context db = new Context())
-            {
-                // Retrieve the student with the specified ID
-                Course course = db.Course.FirstOrDefault(s => s.CourseID == courseID);
-
-                if (course != null)
-                {
-                    // Remove the student from the DbSet
-                    db.Course.Remove(course);
-                    
-                    // Save changes to the database
-                    db.SaveChanges();
-
-                    Console.WriteLine($"#{course.CourseID} {course.Name} was deleted successfully.");
-                }
-                else
-                {
-                    Console.WriteLine($"Subject with ID {courseID} not found.");
-                }
-            }
+            //        foreach (var t in course.Teacher)
+            //        {
+            //            Console.WriteLine($"ID: {t.TeacherID} Name:{t.FirstName}");
+            //        }
+            //    }
+            //}
         }
 
     }
---
+++ LINQ-Labb/Utillity/DBMethods.cs
@@ -0,0 +1,264 @@
+﻿using LINQ_Labb.Data;
+using LINQ_Labb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Labb.Utillity
+{
+    internal class DBMethods
+    {
+        public static void DisplayCoursesWithStudents()
+        {
+            using (var db = new Context())
+            {
+                var jTable = db.Course.Include(s => s.Student).ToList();
+
+                foreach (var c in jTable)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("****************************************");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"ID:{c.CourseID} Course: {c.Name}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("****************************************");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    foreach (var s in c.Student)
+                    {
+                        Console.WriteLine($"ID: {s.StudentID} Name: {s.FirstName} {s.LastName}");
+                    }
+                }
+
+
+
+            }
+        }
+        public static void AddManyStudentsToCourse(int startIndex, int stopIndex, int courseID)
+        {
+            Context db = new Context();
+            var students = db.Student;
+            int counter = 0;
+
+            for (int i = startIndex; i <= stopIndex; i++)
+            {
+                Console.WriteLine($"Added StudentID #{i}");
+                AddStudentToCourse(courseID, i);
+                counter++;
+            }
+            Console.WriteLine($"Attempt to add {counter} students has been made and is finished.");
+        }
+
+        public static void DisplayAllCourses()
+        {
+            Context db = new Context();
+            var courses = db.Course.ToList();
+            Console.WriteLine("************************");
+            Console.WriteLine("Displaying all courses registered in school.");
+            foreach (var c in courses)
+            {
+                Console.WriteLine($"#{c.CourseID} Course: {c.Name}");
+            }
+        }
+        public static void DisplayAllSubjects()
+        {
+            Context db = new Context();
+            var subjects = db.Subject.Include(c => c.Course).ToList();
+            Console.WriteLine("************************");
+            Console.WriteLine("Displaying all subjects registered in school.");
+            var courseName = "";
+
+
+            foreach (var s in subjects)
+            {
+                Console.WriteLine($"SubjectID #{s.SubjectID}\nSubject: {s.Name}\nCourseID: {s.CourseID}\nCourse:{s.Course.Name}");
+                Console.WriteLine();
+            }
+        }
+        public static void DisplayAll()
+        {
+            Context db = new Context();
+            var students = db.Student.ToList();
+            var teachers = db.Teacher.ToList();
+
+            Console.WriteLine("************************");
+            Console.WriteLine("Displaing all persons registered in school.");
+            foreach (var s in students)
+            {
+                Console.WriteLine($"#{s.StudentID} Student: {s.FirstName} {s.LastName}");
+            }
+            Console.WriteLine("-----");
+            foreach (var t in teachers)
+            {
+                Console.WriteLine($"#{t.TeacherID} Teachers: {t.FirstName} {t.LastName}");
+            }
+        }
+
+        public static void AddStudent(string fname, string lname)
+        {
+            Context db = new Context();
+            Student student = new Student();
+
+            student.FirstName = fname;
+            student.LastName = lname;
+
+            db.Student.Add(student);
+
+            db.SaveChanges();
+            Console.WriteLine($"#{student.StudentID} {student.FirstName} {student.LastName} was added successfully!");
+        }
+        public static void AddTeacher(string fname, string lname)
+        {
+            Context db = new Context();
+            Teacher teacher = new Teacher();
+
+            teacher.FirstName = fname;
+            teacher.LastName = lname;
+
+            db.Teacher.Add(teacher);
+            db.SaveChanges();
+            Console.WriteLine($"#{teacher.TeacherID} {teacher.FirstName} {teacher.LastName} was added successfully!");
+        }
+        public static void AddCourse(string courseName)
+        {
+            Context db = new Context();
+            Course course = new Course();
+
+            course.Name = courseName;
+
+            db.Course.Add(course);
+            db.SaveChanges();
+
+            Console.WriteLine($"#{course.CourseID} {courseName} was added successfully!");
+        }
+        public static void AddSubject(string subjectName, int courseID)
+        {
+            Context db = new Context();
+
+            Subject subject = new Subject();
+
+            Course courseObj = db.Course.FirstOrDefault(x => x.CourseID == courseID);
+
+            subject.Name = subjectName;
+            subject.CourseID = courseID;
+            subject.Course = courseObj;
+
+            db.Subject.Add(subject);
+            db.SaveChanges();
+
+        }
+
+        public static string AddStudentToCourse(int courseID, int studentID)
+        {
+            using (var db = new Context())
+            {
+                var selectCourse = db.Course.FirstOrDefault(x => x.CourseID == courseID);
+
+                var student = db.Student.FirstOrDefault(s => s.StudentID == studentID);
+
+
+                if (db.Student.Contains(db.Student.FirstOrDefault(s => s.StudentID == studentID)))
+                {
+                    selectCourse.Student = new List<Student> 
+                    { 
+                        student
+                    };
+
+
+                    db.SaveChanges();
+                    Console.WriteLine($"Sucessfully added Student: ID# {student.StudentID}, Name: {student.FirstName}");
+                    return "Successfully added.";
+                }
+
+                Console.WriteLine($"Could not add Student: ID# {student.StudentID}, Name: {student.FirstName}");
+
+                return "Error, could not add.";
+
+            }
+
+
+
+        }
+        public static string AddTeacherToCourse(int courseID, int teacherID)
+        {
+            using (Context db = new Context())
+            {
+                Course selectCourse = db.Course.FirstOrDefault(x => x.CourseID == courseID);
+
+                Teacher teacher = db.Teacher.FirstOrDefault(s => s.TeacherID == teacherID);
+
+
+                if (db.Teacher.Contains(db.Teacher.FirstOrDefault(s => s.TeacherID == teacherID)))
+                {
+                    selectCourse.Teacher = new List<Teacher>
+                    {
+                        teacher
+                    };
+
+
+                    db.SaveChanges();
+                    Console.WriteLine($"Sucessfully added Teacher: ID# {teacher.TeacherID}, Name: {teacher.FirstName}");
+                    return "Successfully added.";
+                }
+
+                Console.WriteLine($"Could not add Teacher: ID# {teacher.TeacherID}, Name: {teacher.FirstName}");
+
+                return "Error, could not add.";
+
+            }
+
+
+
+        }
+        public static void DeleteSubject(int subjectID)
+        {
+            using (Context db = new Context())
+            {
+                // Retrieve the student with the specified ID
+                Subject subject = db.Subject.FirstOrDefault(s => s.SubjectID == subjectID);
+
+                if (subject != null)
+                {
+                    // Remove the student from the DbSet
+                    db.Subject.Remove(subject);
+
+                    // Save changes to the database
+                    db.SaveChanges();
+
+
+                    Console.WriteLine($"#{subject.SubjectID} {subject.Name} was deleted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine($"Subject with ID {subjectID} not found.");
+                }
+            }
+        }
+        public static void DeleteCourse(int courseID)
+        {
+            using (Context db = new Context())
+            {
+                // Retrieve the student with the specified ID
+                Course course = db.Course.FirstOrDefault(s => s.CourseID == courseID);
+
+                if (course != null)
+                {
+                    // Remove the student from the DbSet
+                    db.Course.Remove(course);
+
+                    // Save changes to the database
+                    db.SaveChanges();
+
+                    Console.WriteLine($"#{course.CourseID} {course.Name} was deleted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine($"Subject with ID {courseID} not found.");
+                }
+            }
+        }
+    }
+}
